6f124fe810e34b366aa9c113d72e96bdd2203e1b
lazarodamasceno <lazarolawrence2@gmail.com>
2024-03-05 22:15:22 -0300

    Created two DTOs

---
+++ v1/src/main/java/com/api/v1/dtos/DateDTO.java
@@ -0,0 +1,17 @@
+package com.api.v1.dtos;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import jakarta.validation.constraints.Pattern;
+
+public record DateDTO(
+    @Pattern(regexp = "[\\d]{2}/[\\d]{2}/[\\d]{4}") String date)
+{
+    
+    public LocalDate get() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return LocalDate.parse(date, dtf);
+    }
+
+}
